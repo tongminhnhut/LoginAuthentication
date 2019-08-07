41cd0493469ce03ef5b6f15bd00164d479426368
Tống Minh Nhựt <tongminhnhut@gmail.com>
2019-08-07 23:48:01 +0700

    + commit branch develop

--- app/src/main/java/com/tongminhnhut/logindemo/MainActivity.kt
+++ app/src/main/java/com/tongminhnhut/logindemo/MainActivity.kt
@@ -3,30 +3,18 @@ package com.tongminhnhut.logindemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.facebook.appevents.AppEventsLogger
 import kotlinx.android.synthetic.main.activity_main.*
 import com.facebook.login.LoginResult
 import com.facebook.login.LoginManager
 import android.content.Intent
-import android.telecom.Call
 import android.util.Log
 import android.view.View
-import com.bumptech.glide.Glide
 import com.facebook.*
 import java.util.*
-import org.json.JSONException
-import com.facebook.Profile.getCurrentProfile
-import com.facebook.internal.ImageRequest.getProfilePictureUri
-import com.facebook.GraphResponse
-import org.json.JSONObject
-import com.facebook.GraphRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import android.R.attr.data
-import android.app.PendingIntent.getActivity
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.tasks.Task
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
@@ -53,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        // [END config_signin]
+
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
@@ -63,40 +51,6 @@ class MainActivity : AppCompatActivity() {
         initLoginGoogle()
 
 
-//        btnFacebook.setOnClickListener {
-//            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
-//            LoginManager.getInstance().registerCallback(callbackManager,
-//                object : FacebookCallback<LoginResult> {
-//                    override fun onSuccess(loginResult: LoginResult) {
-//                        Toast.makeText(this@MainActivity, "Login success", Toast.LENGTH_SHORT).show()
-//                        btnnLogoutFacebook.visibility = View.VISIBLE
-//                        btnFacebook.visibility = View.GONE
-//                        if (loginResult.accessToken != null && !loginResult.accessToken.isExpired){
-//                            val profile = Profile.getCurrentProfile()
-//                            if (profile != null){
-//                                val intent = Intent(this@MainActivity, ProfileActivity::class.java)
-//                                intent.putExtra(PROFILE, profile)
-//                                startActivity(intent)
-////                                setDataLogin(profile)
-//
-//                            }
-//                        }
-//
-//                    }
-//
-//                    override fun onCancel() {
-//                        // App code
-//                    }
-//
-//                    override fun onError(exception: FacebookException) {
-//                        // App code
-//                        Toast.makeText(this@MainActivity, "Login error:" + exception, Toast.LENGTH_SHORT).show()
-//
-//                    }
-//                })
-//        }
-
-//        initLoginGoogle()
 
         val accessToken = AccessToken.getCurrentAccessToken()
         val isLoggedIn = accessToken != null && !accessToken.isExpired
@@ -111,7 +65,6 @@ class MainActivity : AppCompatActivity() {
 
         btnLogoutGoogle.setOnClickListener {
             googleSignInClient.signOut()
-//            firebaseAuth.signOut()
             btnGoogle.visibility = View.VISIBLE
             btnLogoutGoogle.visibility = View.INVISIBLE
         }
@@ -126,6 +79,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initLoginGoogle() {
         btnGoogle.setOnClickListener {
+            progress_bar.visibility = View.VISIBLE
             signIn()
         }
     }
@@ -133,24 +87,14 @@ class MainActivity : AppCompatActivity() {
     private fun initLoginFacebook() {
 
         btnFacebook.setOnClickListener {
+            progress_bar.visibility = View.VISIBLE
             LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email","public_profile"))
             LoginManager.getInstance().registerCallback(callbackManager,
                 object : FacebookCallback<LoginResult> {
                     override fun onSuccess(loginResult: LoginResult) {
                         handleFacebookAccessToken(loginResult.accessToken)
-//                        Toast.makeText(this@MainActivity, "Login success", Toast.LENGTH_SHORT).show()
                         btnnLogoutFacebook.visibility = View.VISIBLE
                         btnFacebook.visibility = View.INVISIBLE
-//                        if (loginResult.accessToken != null && !loginResult.accessToken.isExpired){
-//                            val profile = Profile.getCurrentProfile()
-//                            if (profile != null){
-//                                val intent = Intent(this@MainActivity, ProfileActivity::class.java)
-//                                intent.putExtra(PROFILE, profile)
-//                                startActivity(intent)
-////                                setDataLogin(profile)
-//
-//                            }
-//                        }
 
                     }
 
@@ -172,18 +116,21 @@ class MainActivity : AppCompatActivity() {
         val credential = FacebookAuthProvider.getCredential(accessToken!!.token)
         firebaseAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
+                progress_bar.visibility = View.GONE
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("LLLLLLLLL", "signInWithCredential:success")
+                    val acct = task.result?.user
+                    val photUrl = acct?.getPhotoUrl()!!.toString()
                     val user = firebaseAuth.currentUser
+                    val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+                    intent.putExtra(PROFILE, user)
+                    intent.putExtra(URL_PHOTO, photUrl)
+                    startActivity(intent)
+
                 Toast.makeText(this@MainActivity, "Login success:" + user?.displayName, Toast.LENGTH_SHORT).show()
                 } else {
-                    Log.w("LLLLLLLLL", "signInWithCredential:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
                 }
-
-                // ...
             }
     }
 
@@ -206,11 +153,18 @@ class MainActivity : AppCompatActivity() {
         val credential = GoogleAuthProvider.getCredential(account.idToken, null)
         firebaseAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
+                progress_bar.visibility = View.GONE
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("PPPPPPPPP", "signInWithCredential:success")
-                    val user = firebaseAuth.currentUser?.displayName
-                    Toast.makeText(this@MainActivity, "Login google: "+user, Toast.LENGTH_SHORT).show()
+                    val acct = task.result?.user
+                    val photUrl = acct?.getPhotoUrl()!!.toString()
+                    val user = firebaseAuth.currentUser
+                    val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+                    intent.putExtra(URL_PHOTO, photUrl)
+                    intent.putExtra(PROFILE, user)
+                    startActivity(intent)
+
+
+                    Toast.makeText(this@MainActivity, "Login google: "+ user?.displayName, Toast.LENGTH_SHORT).show()
                     btnGoogle.visibility = View.INVISIBLE
                     btnLogoutGoogle.visibility = View.VISIBLE
 
@@ -223,6 +177,7 @@ class MainActivity : AppCompatActivity() {
 
 
     companion object {
+        const val URL_PHOTO ="url-photo"
         const val PROFILE = "profile"
         const val RC_SIGN_IN = 1000
     }
